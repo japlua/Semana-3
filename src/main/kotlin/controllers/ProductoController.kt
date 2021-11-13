fd0d383542258ba091b9f9c84ff20b4f1aec5725
japlua <83932248+japlua@users.noreply.github.com>
2021-11-13 13:21:26 -0500

    Update ProductoController.kt

--- src/main/kotlin/controllers/ProductoController.kt
+++ src/main/kotlin/controllers/ProductoController.kt
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/diets")
+@RequestMapping("/producto")
 @CrossOrigin(methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.PUT])
 
 class ProductoController {
